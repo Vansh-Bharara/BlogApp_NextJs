745bcca741004b43efc477e7cba04a1d7a331839
Vanssh Barara <vansshbarara@Vansshs-MacBook-Air.local>
2025-07-28 20:58:34 +0530

    deployement scripts updated

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ async function BlogPosts() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {data.map((item: any) => (
+      {data.map((item) => (
         <BlogPostCard data={item} key={item.id} />
       ))}
     </div>
